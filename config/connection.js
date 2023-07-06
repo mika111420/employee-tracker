e1d30854c706d9d9f7391912198f7c49c093840f
Mikayla <user@users-MBP.attlocal.net>
2023-07-06 15:26:29 -0400

    established connection in connection.js file and required dotenvfor credential privacy

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,18 @@
 const mysql = require('mysql2')
 require(dotenv).config();
 
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 3306,
+    database: process.env.DB_NAME
+  })
+  
+  connection.connect(function(err){
+    if(err){
+      throw err
+    }
+    console.log('connected')
+  })
+  module.exports = connection
